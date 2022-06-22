683848f760172f3d0f4597957df30abb864f3948
aerodyne <iqbal.rahman@aerodyne.group>
2022-06-22 23:30:42 +0800

    update header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <div>
+            <span className="header">
+      
+            <img src="https://acm-assets.eco.astro.com.my/images/astro-logo-white.svg" alt="Astro" />
+
+            
+            </span>;
+        </div>
+    )
+
+};
+
+export default Header;
