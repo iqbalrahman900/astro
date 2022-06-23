67bdde35c38d8a4c5d2f803581e86f5f12cc9aa0
aerodyne <iqbal.rahman@aerodyne.group>
2022-06-23 11:52:40 +0800

    Update css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 // import logo from './logo.svg';
+import { Container } from '@mui/system';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
+import SimpleBottomNavigation from './components/MainNav';
 
 function App() {
   return (
-    <div className="App">
-     <Header />
-    </div>
+    <BrowserRouter>
+      <Header />
+     <div className="app">
+      <Container>
+        List of Channel
+      </Container>
+     </div>
+
+     <SimpleBottomNavigation />
+    </BrowserRouter>
+ 
   );
 }
 
